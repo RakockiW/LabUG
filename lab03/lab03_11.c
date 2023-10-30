5dde136a36d7858dbe72e1d4f963fe1905645f4e
Wiktor Rakocki <w.rakocki.064@studms.ug.edu.pl>
2023-10-30 20:20:01 +0100

    Zadanie 11 - poprawka

--- lab03/lab03_11.c
+++ lab03/lab03_11.c
@@ -6,10 +6,10 @@ int main() {
     printf("Podaj wymiary odcinków: \n");
     scanf("%d%d%d", &a, &b, &c);
 
-    if ((a + b > c) && (a + c > b) && (b + c > a)) {
-        printf("Z tych odcinków można utworzyć trójkąt. \n");
+    if ((a * a + b * b == c * c) || (b * b + c * c == a * a) || (a * a + c * c == b * b)) {
+        printf("Z tych odcinków można utworzyć trójkąt prostokątny. \n");
     } else {
-        printf("Z tych odcinków nie da się utworzyć trójkąta. \n");
+        printf("Z tych odcinków nie da się utworzyć trójkąta prostokątnego. \n");
     }
 
     return 0;
