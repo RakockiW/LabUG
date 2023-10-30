83b2ba52a00ca7da1655e62529bb8906b91ebf36
Wiktor Rakocki <w.rakocki.064@studms.ug.edu.pl>
2023-10-30 20:18:34 +0100

    Zadanie 11

---
+++ lab03/lab03_11.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Podaj wymiary odcinków: \n");
+    scanf("%d%d%d", &a, &b, &c);
+
+    if ((a + b > c) && (a + c > b) && (b + c > a)) {
+        printf("Z tych odcinków można utworzyć trójkąt. \n");
+    } else {
+        printf("Z tych odcinków nie da się utworzyć trójkąta. \n");
+    }
+
+    return 0;
+}
