c9e8c5c3a615eaa0d5c380672182d9f4e3b8b95c
Wiktor Rakocki <w.rakocki.064@studms.ug.edu.pl>
2023-10-30 20:51:17 +0100

    Zadanie 14

---
+++ lab03/lab03_14.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+    int kat;
+    float rad;
+    printf("Podaj wartość kąta w stopniach: ");
+    scanf("%d", &kat);
+
+    rad = (float)kat / 180;
+
+    printf("Wartość kąta w radianach wynosi %f Pi rad.", rad);
+}
