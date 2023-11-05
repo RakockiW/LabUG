cf2ba26c5f69622accd3d495289bbfa240cf727a
Wiktor Rakocki <wrakocki@linux-A005-14.inf.ug.edu.pl>
2023-11-05 15:22:26 +0100

    Zadanie 4.2 - poprawka

--- lab04/lab04_02.c
+++ lab04/lab04_02.c
@@ -5,8 +5,13 @@ int main ()
     int a,b;
     printf("Podaj dwie liczby naturalne:\n");
     scanf("%d%d", &a, &b);
+    int wynik = a;
 
-    for (int i = 0; i<b; i++) {
-        a=a*a;
+    for (int i = 1; i<b; i++) {
+        wynik=wynik*a;
     }
+
+    printf("%d do potęgi %d to: %d\n", a, b, wynik);
+
+    return 0;
 }
