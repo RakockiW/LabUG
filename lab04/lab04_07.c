2477e3414c61693c8ab00281b83dc49e99b7404b
Wiktor Rakocki <w.rakocki.064@studms.ug.edu.pl>
2023-11-06 11:14:36 +0100

    Zadanie 4.7

---
+++ lab04/lab04_07.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int n;
+    printf("Podaj liczbę: \n");
+    
+    scanf("%d", &n);
+    
+    double pierwiastek = sqrt(n); 
+
+    int reszta;
+
+    for (int i=2; i <= sqrt(n); i++) {
+        reszta = n % i;
+        if (reszta == 0) {
+            printf("Jest to liczba złożona. \n");
+            break;
+        }
+    }
+
+    if (reszta != 0) {
+        printf("Jest to liczba pierwsza\n");
+    }
+
+
+    return 0;
+}
