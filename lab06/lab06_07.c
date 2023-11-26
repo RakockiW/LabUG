39a70a73ca602d979af2acbb5b97d4c0d450d911
Wiktor Rakocki <wrakocki@linux-A001-14.inf.ug.edu.pl>
2023-11-26 15:21:32 +0100

    Zadania 6.07 6.08 - wersje robocze

---
+++ lab06/lab06_07.c
@@ -0,0 +1,20 @@
+//WERSJA ROBOCZA
+
+#include <stdio.h>
+
+int przeszukiwanie(int tab[10], int n) {
+    if (n < sizeof(tab)/8) {
+
+    } else {
+
+    }
+}
+
+int main() {
+    int n;
+    int tab[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    printf("Podaj liczbę: \n");
+    scanf("%d", &n);
+    return 0;
+}
