8145cafe2447be744b49c864286ce49ad22e089e
Wiktor Rakocki <w.rakocki.064@studms.ug.edu.pl>
2023-12-07 22:46:44 +0100

    Zadanie 6.07 - wersja robocza

--- lab06/lab06_07.c
+++ lab06/lab06_07.c
@@ -2,19 +2,33 @@
 
 #include <stdio.h>
 
-int przeszukiwanie(int tab[10], int n) {
-    if (n < sizeof(tab)/8) {
+int przeszukiwanie(int tab[], int poczatek, int koniec, int szukana) {
+    if (poczatek > koniec) {
+        return -1;
+    }
 
+    int srodek = (poczatek+koniec) / 2;
+
+    if (tab[srodek] == szukana) {
+        return 1;
+    }
+
+    if (srodek > szukana) {
+        return przeszukiwanie(tab, poczatek, srodek-1, szukana);
     } else {
-
+        return przeszukiwanie(tab, srodek+1, koniec, szukana);
     }
 }
 
 int main() {
-    int n;
     int tab[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-    printf("Podaj liczbę: \n");
-    scanf("%d", &n);
+    int szukana;
+    printf("Podaj liczbę:\n");
+    scanf("%d", &szukana);
+    
+    int rozmiar_tablicy = sizeof(tab) / sizeof(tab[0]); 
+    int wynik = przeszukiwanie(tab, 0, rozmiar_tablicy-1, szukana);
+    
+    printf("%d\n", wynik);
     return 0;
 }
