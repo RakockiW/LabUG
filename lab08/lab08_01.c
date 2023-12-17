488ed51d4f0a2afd8e261b0c50c1443c024bc309
Wiktor Rakocki <wrakocki@linux-A001-18.inf.ug.edu.pl>
2023-12-17 12:56:04 +0100

    Zadanie 8.01 - poprawka

--- lab08/lab08_01.c
+++ lab08/lab08_01.c
@@ -6,6 +6,7 @@ int przeszukiwanie_liniowe(int tab[ROZMIAR], int n) {
     for (int i = 0; i <= ROZMIAR; i++) {
         if (tab[i] == n) {
             wynik = 1;
+            return wynik;
         }
     }
     return wynik;
