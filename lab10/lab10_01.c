5f12de78cdab7abbcd9001a863ff6633606041f9
Wiktor Rakocki <wrakocki@linux-A001-18.inf.ug.edu.pl>
2024-01-21 12:49:28 +0100

    Zadanie 10.01

---
+++ lab10/lab10_01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    FILE *fp = fopen("/home/LABPK/wrakocki/plik.txt", "r");
+
+    if (fp == NULL) {
+        printf("Plik plik.txt nie istnieje.\n");
+    } else {
+        printf("Plik plik.txt istnieje.\n");
+    }
+
+    return 0;
+}
